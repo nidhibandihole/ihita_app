f25f729ebcde6d9ef00e05d84e819ee1b37a0b36
Nidhi Bandihole Prasanna <156745690+nidhibandihole@users.noreply.github.com>
2025-08-09 18:49:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import pymysql
 conn = pymysql.connect(
     host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
     port=4000,
-    user='3v5cUKca8NJCP7r.root',
-    password='I6l7xbsgvFbsqUYZ',
+    user='',
+    password='',
     database='test',
     ssl_ca=r"C:\Users\nidhi\OneDrive\Documents\projs\ihita_app\isrgrootx1.pem.txt",
     ssl_verify_cert=True
